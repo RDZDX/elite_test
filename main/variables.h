ba60e48a8555e6d18d311dae90c5874b200f0520
Audrius Puplinskas <audrius@ipk.lt>
2026-07-02 13:35:25 +0300

    Update variables.h

--- main/variables.h
+++ main/variables.h
@@ -4,8 +4,18 @@
 #include <stdbool.h>
 #include "platform.h"
 
+static const VMWCHAR save_file_name[] =
+{
+    'E', ':', '\\',
+    'e','l','i','t','e',
+    '.',
+    's','a','v',
+    0
+};
+
 /* Save file path on Nokia 225 SD card */
-#define SAVE_VAR_NAME L"E:\\elite.sav"
+//#define SAVE_VAR_NAME L"E:\\elite.sav"
+#define SAVE_VAR_NAME save_file_name
 
 /* Nokia 225 screen: 128x160 portrait.
    screen_w and screen_h are set at runtime from vm_graphic_get_screen_width/height(). */
