ef609add643b883c2c32daee0b92e82b0ec68cdb
Audrius Puplinskas <audrius@ipk.lt>
2026-07-03 20:20:59 +0300

    Delete main/trig.h

--- main/trig.h
+++
@@ -1,7 +0,0 @@
-#ifndef trig_include_file
-#define trig_include_file
-
-signed int trig_cos(unsigned char const angle); // 64 for a full circle
-signed int trig_sin(unsigned char const angle); // 64 for a full circle
-
-#endif
